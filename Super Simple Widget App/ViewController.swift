fbc70da4c79b9f8a8c3079e83c9396472eb4ab63
Peter Cimring <pcimring@gmail.com>
2020-06-09 08:44:20 +0300

    Initial Commit

---
+++ Super Simple Widget App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Super Simple Widget App
+//
+//  Created by Peter Cimring on 09/06/2020.
+//  Copyright © 2020 com.pcimring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
