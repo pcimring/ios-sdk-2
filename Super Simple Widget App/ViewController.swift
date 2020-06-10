52e17a805a5294edb25a9b4fc47ca972eb91e080
Peter Cimring <pcimring@gmail.com>
2020-06-10 12:17:15 +0300

    Widget

--- Super Simple Widget App/ViewController.swift
+++ Super Simple Widget App/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import TaboolaSDK
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
+        // Is this still relevant?
+        // let publisherInfo = PublisherInfo.init(publisherId: "sdk-tester-demo")
+        // Taboola.initWith(publisherInfo)
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        // Instantiate the Widget and add it to the View...
+        let myFrame = CGRect(x: (view.bounds.size.width - 200)/2, y: 140, width: 200, height: 150) // let's center our Widget
+        let taboolaView = TaboolaView(frame: myFrame)
+        view.addSubview(taboolaView)
+        
+        // Set the view properties...
+                
+        // The Widget Template ID
+        taboolaView.mode = "alternating-widget-without-video-1x4"
+
+        // Your Account ID
+        taboolaView.publisher = "sdk-tester-demo"
+
+        // The Page Type on which the widget will display (article, photo, video, home, categor or search) If you're not sure, contact your account manager.
+        taboolaView.pageType = "article"
+
+        // The Canonical URL for the page on which the Widget will display
+        taboolaView.pageUrl = "https://blog.taboola.com"
+
+        // Is this still relevant?
+        //(Optional) Your internal ID for the page on which the Widget will display
+        //taboolaView.pageId = "my-page-id"
+
+        // The Placement ID for the Widget
+        taboolaView.placement = "Below Article"
+
+        //The Target Type of the page on which the widget is displayed. If you're not sure, contact your account manager.
+        taboolaView.targetType = "mix"
+        
+        // Populate the Widget
+        taboolaView.fetchContent()
+        
     }
-
-
 }
 
